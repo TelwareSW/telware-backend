1f6034ac5b1fe7fd421500befebda7621b6fcad6
akramhany <akramhany65@gmail.com>
2024-10-23 16:46:55 +0300

    feat(userProfile): implemente retrieving user(s) data

--- src/routes/apiRoute.ts
+++ src/routes/apiRoute.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import authRouter from '@routes/authRoutes';
+import userRouter from '@routes/userRoute';
 
 const apiRouter = Router();
 
 apiRouter.use('/auth', authRouter);
+
+apiRouter.use('/users', userRouter);
 
 export default apiRouter;
