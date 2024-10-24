e01b85f71e69cefb249629bda89f067590921dff
somiaelshemy <66778747+somiaelshemy@users.noreply.github.com>
2024-10-24 20:33:43 +0300

    feat(auth): add I'm not a robot verification
    * feat(verify): set verification setup
    * feat(auth): add im not a robot verification

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,10 +2,25 @@ import catchAsync from '@base/utils/catchAsync';
 import User from '@base/models/userModel';
 import { Request, Response, NextFunction } from 'express';
 import generateUsername from '@utils/generateUsername';
+import {
+  verifyReCaptcha,
+  IReCaptchaResponse,
+} from '@base/utils/verifyReCaptcha';
 
 export const signup = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const { email, phoneNumber, password, passwordConfirm } = req.body;
+    const { email, phoneNumber, password, passwordConfirm, recaptchaResponse } =
+      req.body;
+
+    const reCaptchaMessageResponse: IReCaptchaResponse =
+      await verifyReCaptcha(recaptchaResponse);
+
+    if (reCaptchaMessageResponse.response === 400)
+      res.status(400).json({
+        status: 'fail',
+        message: reCaptchaMessageResponse.message,
+      });
+
     const username: string = await generateUsername();
 
     await User.create({
@@ -17,7 +32,8 @@ export const signup = catchAsync(
     });
 
     res.status(201).json({
-      message: 'User is created. Please verify your account',
+      status: 'success',
+      message: `${reCaptchaMessageResponse.message} and User is created successfuly. Please verify your account`,
       data: {},
     });
   }
