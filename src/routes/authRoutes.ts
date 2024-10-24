f9081d11b6ef27db8d395a6e4ee4bd2185528633
somiaelshemy <somiaelshemy22@gmail.com>
2024-10-24 23:34:00 +0300

    feature(auth): add verify email

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,13 @@
 import express from 'express';
-import { signup, sendConfirmationCode } from '@controllers/authController';
+import {
+  signup,
+  sendConfirmationCode,
+  verifyEmail,
+} from '@controllers/authController';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/send-confirmation', sendConfirmationCode);
+router.post('/verify', verifyEmail);
 export default router;
