644d1094e73e3619246469a8bd8fe8badecd11d0
akramhany <akramhany65@gmail.com>
2024-10-26 16:47:33 +0300

    feat(uploads): config multer for file uploads

---
+++ src/config/file_uploads.ts
@@ -0,0 +1,15 @@
+import multer from 'multer';
+
+// Define storage for uploaded files
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+export default upload;
