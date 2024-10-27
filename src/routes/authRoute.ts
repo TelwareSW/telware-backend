7cca50fcff3e0580929d2a4c9edb74fe62e767a2
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2024-10-27 19:59:45 +0300

    feat(auth): add me route

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -5,7 +5,9 @@ import {
   verifyEmail,
   login,
   refresh,
+  isLoggedIn,
 } from '@controllers/authController';
+import {protect} from '@middlewares/authMiddleware';
 import oauthRouter from '@base/routes/oauthRoute';
 
 const router = Router();
@@ -16,6 +18,7 @@ router.post('/signup', signup);
 router.post('/login', login);
 router.post('/send-confirmation', sendConfirmationCode);
 router.post('/verify', verifyEmail);
-router.post('/refresh', refresh);
+router.post('/refresh', protect, refresh);
+router.get('/me', protect, isLoggedIn);
 
 export default router;
