d77332ae1d3108ad970c1cec326c378882503682
akramhany <akramhany65@gmail.com>
2024-10-28 00:56:25 +0300

    feat(story-view): implement viewing a story for a user
    asdfl
    
    feat(story-view): implement viewing a story for a user

--- src/routes/apiRoute.ts
+++ src/routes/apiRoute.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRouter from '@routes/authRoutes';
 import userRouter from '@routes/userRoutes';
+import storyRouter from '@routes/storyRoutes';
 
 const apiRouter = Router();
 
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/users', userRouter);
+apiRouter.use('/stories', storyRouter);
 
 export default apiRouter;
