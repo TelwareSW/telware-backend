4ffe5c878b3cae435786e8f0b0005d1a4f217b63
akramhany <akramhany65@gmail.com>
2024-10-27 15:21:23 +0300

    feat(stories): implement an endpoint to upload stories

--- src/config/file_uploads.ts
+++ src/config/file_uploads.ts
@@ -1,15 +1,24 @@
 import multer from 'multer';
+import path from 'path';
+import crypto from 'crypto';
 
 // Define storage for uploaded files
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, path.join(process.cwd(), 'src/public/uploads'));
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    const uniqueSuffix = crypto.randomBytes(8).toString('hex');
+    const ext = path.extname(file.originalname);
+    const fileName = `${uniqueSuffix}${ext}`;
+
+    cb(null, fileName);
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+});
 
 export default upload;
