d2c1533d668bf30dc78a9adabb4d9b8441cc63b5
akramhany <akramhany65@gmail.com>
2024-10-28 19:10:42 +0300

    fix(user-routes): fix user routes to be more convenient

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,11 +1,7 @@
 import { Router } from 'express';
 import upload from '@base/config/fileUploads';
 import privacyRouter from '@routes/privacyRoute';
-import {
-  block,
-  getBlockedUsers,
-  unblock,
-} from '@controllers/privacyController';
+import { block, getBlockedUsers, unblock } from '@controllers/privacyController';
 import {
   getAllUsers,
   getUser,
@@ -26,6 +22,17 @@ const router = Router();
 
 router.use('/privacy', privacyRouter);
 
+// Stories routes
+router.get('/stories', getCurrentUserStory);
+router.post('/stories', upload.single('file'), postStory);
+router.delete('/stories/:storyId', deleteStory);
+router.get('/:userId/stories', getStory);
+
+// Block settings
+router.get('/block', getBlockedUsers);
+router.post('/block/:id', block);
+router.delete('/block/:id', unblock);
+
 // User routes
 router.get('/', getAllUsers);
 router.get('/me', getUser);
@@ -35,16 +42,5 @@ router.patch('/phone', updatePhoneNumber);
 router.patch('/email', updateEmail);
 router.patch('/username', updateUsername);
 router.patch('/screen-name', updateScreenName);
-
-// Block settings
-router.get('/block', getBlockedUsers);
-router.post('/block/:id', block);
-router.delete('/block/:id', unblock);
-
-// Stories routes
-router.get('/me/stories', getCurrentUserStory);
-router.post('/me/stories', upload.single('file'), postStory);
-router.delete('/me/stories/:storyId', deleteStory);
-router.get('/:userId/stories', getStory);
 
 export default router;
