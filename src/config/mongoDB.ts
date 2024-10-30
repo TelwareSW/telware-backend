c194a3448362a351cebeef47164a0786aea2a05a
AhmedHamed3699 <ahmed.hamed03@eng-st.cu.edu.eg>
2024-10-30 18:41:48 +0300

    fix(docker): solved docker problem with mongo

--- src/config/mongoDB.ts
+++ src/config/mongoDB.ts
@@ -1,5 +1,13 @@
 import mongoose from 'mongoose';
 
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected');
+});
+
 async function mongoDBConnection(MONGO_URI: string) {
   try {
     await mongoose.connect(MONGO_URI);
