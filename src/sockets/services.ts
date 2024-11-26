c8ac40828e50e218a84bb1d5c6a44d222e33b697
somiaelshemy <somiaelshemy22@gmail.com>
2024-11-26 15:18:35 +0200

    feat(message): delete/edit message

--- src/sockets/services.ts
+++ src/sockets/services.ts
@@ -2,8 +2,7 @@ import { Socket } from 'socket.io';
 import Message from '@base/models/messageModel';
 import { createChat } from '@base/services/chatService';
 
-export const handleSendMessage = async (io: any, socket: Socket, data: any) => {
-  console.log('inside handleSendMessage');
+export const handleSendMessage = async (socket: Socket, data: any) => {
   let { chatId } = data;
   const { content, contentType, senderId, isFirstTime } = data;
   let newChat;
@@ -20,19 +19,30 @@ export const handleSendMessage = async (io: any, socket: Socket, data: any) => {
     await message.save();
 
     if (isFirstTime) {
-      const members = [chatId,senderId];
+      const members = [chatId, senderId];
       newChat = await createChat(members);
 
       chatId = newChat._id;
       socket.join(chatId);
     }
-    io.to(chatId).emit('RECEIVE_MESSAGE', message);
+    socket.to(chatId).emit('RECEIVE_MESSAGE', message);
   } catch (error) {
     console.error('Error_SEND_MESSAGE:', error);
     socket.emit('ERROR', { message: 'Failed to send message.' });
   }
 };
 
-export const handleReceiveMessage = async () => {
+export const handleEditMessage = async (socket: Socket, data: any) => {
+  const { messageId, content } = data;
+  Message.findByIdAndUpdate(messageId, { content });
+
+
+};
+
+
+export const handleDeleteMessage = async (socket: Socket, data: any) => {
+  const { messageId, deleteType } = data;
+  if(deleteType === 'all')
+    Message.findByIdAndDelete(messageId);
   
 };
--- src/sockets/socket.ts
+++ src/sockets/socket.ts
@@ -1,32 +1,31 @@
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { Server as HTTPServer } from 'http';
 import corsOptions from '@base/config/cors';
 import registerChatHandlers from '@base/sockets/chat';
 import { getAllChatIds } from '@services/chatService';
 import mongoose from 'mongoose';
-import { handleReceiveMessage, handleSendMessage } from './services';
+import { handleSendMessage, handleEditMessage } from './services';
+
+const joinRooms = async (socket: Socket, userId: mongoose.Types.ObjectId) => {
+  const chatIds = await getAllChatIds(userId);
+  chatIds.forEach((chatId: mongoose.Types.ObjectId) => {
+    socket.join(chatId.toString());
+  });
+};
 
 const socketSetup = (server: HTTPServer) => {
   const io = new Server(server, {
     cors: corsOptions,
   });
-
   io.on('connection', async (socket) => {
     console.log(`New client connected: ${socket.id}`);
 
     const userId = new mongoose.Types.ObjectId(); //TODO: Replace it with the actual userId
-    const chatIds = await getAllChatIds(userId);
+    await joinRooms(socket, userId);
 
-    chatIds.forEach((chatId: mongoose.Types.ObjectId) => {
-      socket.join(chatId.toString());
-    });
-
-    socket.on('SEND_MESSAGE', (message) =>
-      handleSendMessage(io, socket, message)
-    );
-
-    socket.on('RECEIVE_MESSAGE', () => handleReceiveMessage());
-
+    socket.on('SEND_MESSAGE', (data: any) => handleSendMessage(socket, data));
+    socket.on('EDIT_MESSAGE', (data: any) => handleEditMessage(socket, data));
+    socket.on('DELETE_MESSAGE', (data: any) => handleDeleteMessage(socket, data));
     registerChatHandlers(io, socket);
   });
 };
