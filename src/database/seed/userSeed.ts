3d5e9c2c146c9db33bc8334f2713f29be7f4d05e
Peter Safwat <118059454+PeterSafwatHBK@users.noreply.github.com>
2024-11-29 17:23:38 +0200

    Updated Seeds

--- src/database/seed/userSeed.ts
+++ src/database/seed/userSeed.ts
@@ -1,18 +1,45 @@
 import fs from 'fs';
 import { faker } from '@faker-js/faker';
 import User from '@models/userModel';
+import mongoose from 'mongoose';
 
 const users = JSON.parse(
   fs.readFileSync(`${__dirname}/json/users.json`, 'utf-8')
 );
 
+const createRandomMessage = (sender: mongoose.Types.ObjectId, receiver: mongoose.Types.ObjectId) => ({
+  sender,
+  receiver,
+  content: faker.lorem.sentence(),
+  timestamp: faker.date.recent(),
+  isRead: faker.datatype.boolean(),
+});
+
+const createRandomChat = () => {
+  const user1 = new mongoose.Types.ObjectId();
+  const user2 = new mongoose.Types.ObjectId();
+
+  const messages = Array.from({ length: faker.number.int({ min: 1, max: 5 }) }, () =>
+    createRandomMessage(user1, user2)
+  );
+
+  return {
+    _id: new mongoose.Types.ObjectId(),
+    isMuted: faker.datatype.boolean(),
+    unmuteDuration: faker.number.int({ min: 0, max: 60 }),
+    Draft: faker.lorem.sentence(),
+    messages,
+  };
+};
+
 const createRandomUser = () => {
   const password = faker.internet.password({ length: 12, memorable: true });
 
+  const chats = Array.from({ length: faker.number.int({ min: 0, max: 5 }) }, createRandomChat);
   return {
     email: faker.internet.email(),
     username: faker.internet
-      .username()
+      .userName()
       .replace(/[.\-/\\]/g, '')
       .padEnd(2, '_')
       .padStart(2, '_')
@@ -21,13 +48,14 @@ const createRandomUser = () => {
     password,
     passwordConfirm: password,
     accountStatus: 'active',
+    chats,
   };
 };
 
-const fakerUsers = faker.helpers.multiple(createRandomUser, {
-  count: 20,
-});
+// Generate a list of random users
+const fakerUsers = faker.helpers.multiple(createRandomUser, { count: 20 });
 
+// Import the data into the database
 const importUserData = async () => {
   try {
     const allUsers = [...users, ...fakerUsers];
