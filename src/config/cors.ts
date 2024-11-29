53b31cee7e11abdebdac58ea02f9c449d74f844b
akramhany <akramhany65@gmail.com>
2024-11-29 14:47:49 +0200

    fix(cors): include the allowed origins object in cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,9 +3,9 @@ import { readFileSync } from 'fs';
 const allowedOrigins = JSON.parse(
   readFileSync(`${__dirname}/allowedOrigins.json`, 'utf8')
 );
-
+console.log(allowedOrigins);
 const corsOptions = {
-  origin: 'allowedOrigins',
+  origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true,
   withCredentials: true,
