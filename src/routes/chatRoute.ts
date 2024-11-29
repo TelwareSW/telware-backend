eff8a09ab72ee72965726e72094d2c9bcc9b9e57
akramhany <akramhany65@gmail.com>
2024-11-29 14:46:17 +0200

    feat(media-upload): Make an endpoint to upload media files

--- src/routes/chatRoute.ts
+++ src/routes/chatRoute.ts
@@ -3,16 +3,92 @@ import {
   createChat,
   getAllChats,
   getMessages,
+  postMediaFile,
 } from '@base/controllers/chatController';
 import { protect } from '@base/middlewares/authMiddleware';
 import restrictToMembers from '@base/middlewares/chatMiddlewares';
 
 const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *  name: Chat
+ *  description: The Chat Managing API
+ */
+
 router.use(protect);
 
 router.post('/', createChat);
 router.get('/:type?', getAllChats);
 router.get('/messages/:chatId', restrictToMembers, getMessages);
+
+/**
+ * @swagger
+ * /chats/media:
+ *   post:
+ *     summary: Upload a media file for chats
+ *     description: Uploads a media file to the server and returns the filename of the uploaded file.
+ *     tags: [Chat]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *                 description: The media file to upload
+ *     responses:
+ *       200:
+ *         description: Media file uploaded successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: success
+ *                 message:
+ *                   type: string
+ *                   example: Uploaded media file successfully
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     mediaFileName:
+ *                       type: string
+ *                       description: The name of the uploaded media file
+ *                       example: media-file-name.jpg
+ *       400:
+ *         description: Bad Request. File not provided or invalid.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: fail
+ *                 message:
+ *                   type: string
+ *                   example: An error occurred while uploading the story
+ *       500:
+ *         description: Internal Server Error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: error
+ *                 message:
+ *                   type: string
+ *                   example: An error occurred while uploading the story
+ */
+router.post('/media', upload.single('file'), postMediaFile);
 
 export default router;
