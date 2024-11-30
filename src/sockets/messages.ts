605b2e5d480f6f4be30be5586f491f47d2c512fb
akramhany <akramhany65@gmail.com>
2024-11-30 12:06:18 +0200

    feat(pinMessages): implement socket io pin message events

--- src/sockets/messages.ts
+++ src/sockets/messages.ts
@@ -1,56 +1,103 @@
+import Message from '@base/models/messageModel';
+import mongoose from 'mongoose';
+import { Server, Socket } from 'socket.io';
+
+interface PinMessageData {
+  chatId: string | mongoose.Types.ObjectId;
+  messageId: string | mongoose.Types.ObjectId;
+  userId: string | mongoose.Types.ObjectId;
+}
+
 /**
  * @swagger
  * tags:
- *  name: Messages WebSocket Events
- *  description: The  Managing API
+ *  name: Sockets
+ *  description: The Sockets Managing API
  */
 
 /**
  * @swagger
- * components:
- *   schemas:
- *     PIN_MESSAGE_CLIENT:
- *       type: object
- *       description: Event sent from the client to the server to pin a message in a chat.
- *       properties:
- *         chatId:
- *           type: string
- *           description: The ID of the chat where the message is being pinned
- *           example: "12345"
- *         messageId:
- *           type: string
- *           description: The ID of the message to be pinned
- *           example: "67890"
- *         userId:
- *           type: string
- *           description: The ID of the user performing the action
- *           example: "98765"
- *     PIN_MESSAGE_SERVER:
- *       type: object
- *       description: Event sent from the server to the client to confirm or notify that a message has been pinned in a chat.
- *       properties:
- *         chatId:
- *           type: string
- *           description: The ID of the chat where the message is being pinned
- *           example: "12345"
- *         messageId:
- *           type: string
- *           description: The ID of the message to be pinned
- *           example: "67890"
- *         userId:
- *           type: string
- *           description: The ID of the user performing the action
- *           example: "98765"
- *
- * tags:
- *   - name: Messages WebSocket Events
- *     description: Events for real-time communication using WebSockets.
- *
- * Example Usage:
- *   Client to Server:
- *     event: "PIN_MESSAGE_CLIENT"
- *     payload: { "chatId": "12345", "messageId": "67890", "userId": "98765" }
- *   Server to Client:
- *     event: "PIN_MESSAGE_SERVER"
- *     payload: { "chatId": "12345", "messageId": "67890", "userId": "98765" }
+ * /PIN_MESSAGE_CLIENT:
+ *   post:
+ *     summary: Pins a message in a chat.
+ *     description: Marks a specific message as pinned in a chat. The pinned message information is sent to the server.
+ *     tags: [Sockets]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - chatId
+ *               - messageId
+ *               - userId
+ *             properties:
+ *               chatId:
+ *                 type: string
+ *                 description: The unique ID of the chat where the message will be pinned.
+ *               messageId:
+ *                 type: string
+ *                 description: The unique ID of the message to be pinned.
+ *               userId:
+ *                 type: string
+ *                 description: The unique ID of the user performing the action.
+ *                 example: "98765"
  */
+
+/**
+ * @swagger
+ * /PIN_MESSAGE_SERVER:
+ *   post:
+ *     summary: Notifies clients about a pinned message in a chat.
+ *     description: Sends information to clients that a specific message has been pinned in a chat.
+ *     tags: [Sockets]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - chatId
+ *               - messageId
+ *               - userId
+ *             properties:
+ *               chatId:
+ *                 type: string
+ *                 description: The unique ID of the chat where the message was pinned.
+ *               messageId:
+ *                 type: string
+ *                 description: The unique ID of the message that was pinned.
+ *               userId:
+ *                 type: string
+ *                 description: The unique ID of the user who performed the action.
+ *                 example: "98765"
+ */
+
+async function handlePinMessage(socket: Socket, data: PinMessageData) {
+  try {
+    // Make a message pinned
+    const message = await Message.findById(data.messageId);
+    if (!message) {
+      //TODO: Make a global socket event for the client to send errors to
+      return;
+    }
+
+    message.isPinned = true;
+    await message.save();
+
+    // Send an event to all online chat users to pin a message.
+    socket.to(data.chatId.toString()).emit('PIN_MESSAGE_SERVER', data);
+  } catch (err) {
+    //TODO: Make a global socket event for the client to send errors to
+  }
+}
+
+async function registerMessagesHandlers(io: Server, socket: Socket) {
+  socket.on('PIN_MESSAGE_CLIENT', (data: PinMessageData) =>
+    handlePinMessage(socket, data)
+  );
+}
+
+export default registerMessagesHandlers;
