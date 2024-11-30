e1b8a4c1d0ba5e2e5e9fb2c0a58349eff58b488a
AhmedHamed3699 <ahmed.hamed03@eng-st.cu.edu.eg>
2024-11-30 17:44:13 +0200

    feat(docs): add functional documentation setup

---
+++ docs/functions/assets/search.js
@@ -0,0 +1,1 @@
+window.searchData = "data:application/octet-stream;base64,H4sIAAAAAAAAA6tWKsovL1ayio7VUcrMS0mtULKqVipLLSrOzM9TslIy0jPWs1TSUUrLTM1JASlTykvMTQUKJOfn5qbmlQBZKfnJpWBmLFRZWGpySX4R3EygYSWpKZ4Qs0FCBZkFqTmZeakgXm0tACJTfzuBAAAA";
