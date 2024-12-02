836d52fb0b224ba49dac29b0393e4ca0c453b21a
akramhany <akramhany65@gmail.com>
2024-12-02 16:17:35 +0200

    feat(UnPin): make an event to unpin a message

--- src/sockets/messages.ts
+++ src/sockets/messages.ts
@@ -2,13 +2,13 @@ import Message from '@base/models/messageModel';
 import mongoose from 'mongoose';
 import { Server, Socket } from 'socket.io';
 
-interface PinMessageData {
+interface PinUnPinMessageData {
   chatId: string | mongoose.Types.ObjectId;
   messageId: string | mongoose.Types.ObjectId;
   userId: string | mongoose.Types.ObjectId;
 }
 
-async function handlePinMessage(socket: Socket, data: PinMessageData) {
+async function handlePinMessage(socket: Socket, data: PinUnPinMessageData) {
   try {
     // Make a message pinned
     const message = await Message.findById(data.messageId);
@@ -27,9 +27,32 @@ async function handlePinMessage(socket: Socket, data: PinMessageData) {
   }
 }
 
+async function handleUnPinMessage(socket: Socket, data: PinUnPinMessageData) {
+  try {
+    // Make a message unpinned
+    const message = await Message.findById(data.messageId);
+    if (!message) {
+      //TODO: Make a global socket event for the client to send errors to
+      return;
+    }
+
+    message.isPinned = false;
+    await message.save();
+
+    // Send an event to all online chat users to unpin a message.
+    socket.to(data.chatId.toString()).emit('UNPIN_MESSAGE_SERVER', data);
+  } catch (err) {
+    //TODO: Make a global socket event for the client to send errors to
+  }
+}
+
 async function registerMessagesHandlers(io: Server, socket: Socket) {
-  socket.on('PIN_MESSAGE_CLIENT', (data: PinMessageData) =>
+  socket.on('PIN_MESSAGE_CLIENT', (data: PinUnPinMessageData) =>
     handlePinMessage(socket, data)
+  );
+
+  socket.on('UNPIN_MESSAGE_CLIENT', (data: PinUnPinMessageData) =>
+    handleUnPinMessage(socket, data)
   );
 }
 
--- src/swagger/docs/message.swagger.ts
+++ src/swagger/docs/message.swagger.ts
@@ -37,6 +37,36 @@
 
 /**
  * @swagger
+ * /UNPIN_MESSAGE_CLIENT:
+ *   post:
+ *     summary: unpins a message in a chat.
+ *     description: Change a pinned message to be unpinned. The message information is sent to the server.
+ *     tags: [Sockets]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - chatId
+ *               - messageId
+ *               - userId
+ *             properties:
+ *               chatId:
+ *                 type: string
+ *                 description: The unique ID of the chat where the message will be unpinned.
+ *               messageId:
+ *                 type: string
+ *                 description: The unique ID of the message to be unpinned.
+ *               userId:
+ *                 type: string
+ *                 description: The unique ID of the user performing the action.
+ *                 example: "98765"
+ */
+
+/**
+ * @swagger
  * /PIN_MESSAGE_SERVER:
  *   post:
  *     summary: Notifies clients about a pinned message in a chat.
@@ -59,6 +89,36 @@
  *               messageId:
  *                 type: string
  *                 description: The unique ID of the message that was pinned.
+ *               userId:
+ *                 type: string
+ *                 description: The unique ID of the user who performed the action.
+ *                 example: "98765"
+ */
+
+/**
+ * @swagger
+ * /UNPIN_MESSAGE_SERVER:
+ *   post:
+ *     summary: Notifies clients about an unpinned message in a chat.
+ *     description: Sends information to clients that a specific message has been unpinned in a chat.
+ *     tags: [Sockets]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - chatId
+ *               - messageId
+ *               - userId
+ *             properties:
+ *               chatId:
+ *                 type: string
+ *                 description: The unique ID of the chat where the message was unpinned.
+ *               messageId:
+ *                 type: string
+ *                 description: The unique ID of the message that was unpinned.
  *               userId:
  *                 type: string
  *                 description: The unique ID of the user who performed the action.
