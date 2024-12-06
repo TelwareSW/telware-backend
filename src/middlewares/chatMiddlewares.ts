8022ffac64f03ac59666f44b6370f60654b5c107
somiaelshemy <somiaelshemy22@gmail.com>
2024-12-06 11:52:49 +0200

    edit(middlewares): update restrict to handle roles

--- src/middlewares/chatMiddlewares.ts
+++ src/middlewares/chatMiddlewares.ts
@@ -2,30 +2,44 @@ import AppError from '@base/errors/AppError';
 import Chat from '@base/models/chatModel';
 import IUser from '@base/types/user';
 import { NextFunction, Request, Response } from 'express';
+import mongoose from 'mongoose';
 
-const restrictToMembers = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const { chatId } = req.params;
-  const user: IUser = req.user as IUser;
-  const userId: any = user._id;
-  const chat = await Chat.findById(chatId);
-  if (!chat) return next(new AppError('this chat does no longer exists', 400));
-  const chatMembers = chat.members;
-  if (!chatMembers)
-    return next(
-      new AppError('the chat has no members, you cannot access it anymore', 403)
-    );
-  if (!chatMembers.includes(userId))
-    return next(
-      new AppError(
-        'you are not a member of this chat, you are not allowed here',
-        403
-      )
-    );
-  next();
-};
+interface Member {
+  _id: mongoose.Types.ObjectId;
+  Role: 'member' | 'admin' | 'creator';
+}
 
-export default restrictToMembers;
+const restrictTo =
+  (...roles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { chatId } = req.params;
+    const user: IUser = req.user as IUser;
+    const userId: any = user._id;
+    const chat = await Chat.findById(chatId);
+    if (!chat)
+      return next(new AppError('this chat does no longer exists', 400));
+
+    const chatMembers = chat.members;
+    if (!chatMembers)
+      return next(
+        new AppError(
+          'the chat has no members, you cannot access it anymore',
+          403
+        )
+      );
+    const member: Member = chatMembers.find((m) =>
+      m._id.equals(userId)
+    ) as unknown as Member;
+    if (!member)
+      return next(
+        new AppError(
+          'you are not a member of this chat, you are not allowed here',
+          403
+        )
+      );
+    if (chat.type !== 'private' && !roles.includes(member.Role))
+      return next(new AppError('you do not have permission', 403));
+    next();
+  };
+
+export default restrictTo;
