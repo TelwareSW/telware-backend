c6b6e24c2b2b050429922376d5e7cc45c9cbde29
akramhany <akramhany65@gmail.com>
2024-12-12 00:07:59 +0200

    feat(voice-calls): change the voice calls model and type

--- src/models/voiceCallModel.ts
+++ src/models/voiceCallModel.ts
@@ -3,14 +3,18 @@ import mongoose from 'mongoose';
 import Communication from './communicationModel';
 
 const voiceCallSchema = new mongoose.Schema<IVoiceCall>({
-  duration: {
-    type: Number,
-    default: 0,
+  timestamp: { type: Date, required: true },
+  duration: { type: Number },
+  callType: { type: String, enum: ['group', 'private'], required: true },
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
   },
-  status: {
-    type: String,
-    enum: ['declined', 'answered'],
-  },
+  participants: [
+    { type: mongoose.Types.ObjectId, ref: 'User', required: true, default: [] },
+  ],
+  chatId: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true },
 });
 
 const VoiceCall = Communication.discriminator('VoiceCall', voiceCallSchema);
