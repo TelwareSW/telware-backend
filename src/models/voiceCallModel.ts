a63c29964c47112fcf2ccf19aeeb4cd090f967d2
akramhany <akramhany65@gmail.com>
2024-12-12 02:12:03 +0200

    feat(voice-calls): implement CREATE-CALL event handler

--- src/models/voiceCallModel.ts
+++ src/models/voiceCallModel.ts
@@ -3,17 +3,17 @@ import mongoose from 'mongoose';
 import Communication from './communicationModel';
 
 const voiceCallSchema = new mongoose.Schema<IVoiceCall>({
-  timestamp: { type: Date, required: true },
-  duration: { type: Number },
+  timestamp: { type: Date, default: Date.now },
+  duration: { type: Number, default: -1 },
   callType: { type: String, enum: ['group', 'private'], required: true },
-  status: { type: String, enum: ['ongoing', 'finished'], required: true },
+  status: { type: String, enum: ['ongoing', 'finished'], default: 'ongoing' },
   senderId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true,
   },
   currentParticipants: [
-    { type: mongoose.Types.ObjectId, ref: 'User', required: true, default: [] },
+    { type: mongoose.Types.ObjectId, ref: 'User', default: [] },
   ],
   chatId: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true },
 });
