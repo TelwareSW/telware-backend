4040bfdf186395f3d0e114a655366e520700a91d
Peter Safwat <118059454+PeterSafwatHBK@users.noreply.github.com>
2024-12-20 09:59:04 +0200

    authRoute-updated

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -13,11 +13,11 @@ import {
   getLogedInSessions,
   getCurrentSession,
 } from '@controllers/authController';
-import { protect,isActive } from '@middlewares/authMiddleware';
+import { protect } from '@middlewares/authMiddleware';
+import { isActive } from '@middlewares/authMiddleware';
 import oauthRouter from '@base/routes/oauthRoute';
 
 const router = Router();
-router.use(isActive);
 
 router.use('/oauth', oauthRouter);
 
@@ -28,10 +28,11 @@ router.post('/send-confirmation', sendConfirmationCode);
 router.post('/verify', verifyEmail);
 router.post('/password/forget', forgotPassword);
 router.patch('/password/reset/:token', resetPassword);
-router.patch('/password/change', protect, changePassword);
 
 router.use(protect);
+router.patch('/password/change', protect, changePassword);
 
+router.use(isActive);
 router.get('/me', getCurrentSession);
 router.get('/sessions', getLogedInSessions);
 router.post('/logout', logoutSession);
